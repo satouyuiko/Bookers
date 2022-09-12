4f88b550c94490089a586d9156e9ef5dafdff121
satouyuiko <ec2-user@ip-172-31-12-66.ap-northeast-1.compute.internal>
2022-09-12 15:05:20 +0000

    [Add]詳細画面まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,13 +6,15 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/top'
+    redirect_to book_path(book.id)
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
