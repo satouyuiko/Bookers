b7ff6a5f58dfa415fedea4b125296576da776e0a
satouyuiko <ec2-user@ip-172-31-12-66.ap-northeast-1.compute.internal>
2022-09-15 15:47:58 +0000

    [Add]バリデーションまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,12 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+       redirect_to book_path(@book.id)
+    else
+       render :new
+    end
   end
 
   def index
@@ -19,6 +22,12 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
   
   def update
