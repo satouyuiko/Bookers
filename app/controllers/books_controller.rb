69b72161b0d9fcbafc3d664a46c05863a9ed327c
satouyuiko <ec2-user@ip-172-31-12-66.ap-northeast-1.compute.internal>
2022-09-21 13:45:22 +0000

    [add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,13 +6,18 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-       redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully created."
+      
+       redirect_to "/books/#{@book.id}"
+       
     else
-       render :new
+       @books = Book.all
+       render "index"
     end
   end
 
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -27,13 +32,15 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    redirect_to books_path
   end
   
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book.id)
+    flash[:notice] = "Book was successfully updated."
+    
+    redirect_to "/books/#{book.id}"
   end
     
   private
@@ -41,4 +48,5 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
+  
 end
